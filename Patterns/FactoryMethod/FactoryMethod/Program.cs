fbf80ed81e274b5208a43cd794d3fcebc9ebcbf4
Igor Skirnevskiy <igorskir@yandex.ru>
2015-12-16 13:20:26 +0600

    Реализован паттерн Фабричный метод (Factory Method) на примере строительной компании.

---
+++ Patterns/FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Developer dev = new PanelDeveloper("TDSK_PANEL");
+            House panelHouse = dev.Create();
+
+            dev = new WoodDeveloper("WOOD_STROY");
+            House woodHouse = dev.Create();
+
+            Console.ReadLine();
+        }
+    }
+
+
+    #region Developer Classes
+    internal abstract class Developer
+    {
+        public Developer(string n)
+        {
+            Name = n;
+        }
+
+        public string Name { get; set; }
+
+        // Fabric method
+        public abstract House Create();
+    }
+
+    internal class PanelDeveloper : Developer
+    {
+        public PanelDeveloper(string n)
+            : base(n)
+        {
+        }
+
+        public override House Create()
+        {
+            return new PanelHouse();
+        }
+    }
+
+    internal class WoodDeveloper : Developer
+    {
+        public WoodDeveloper(string n)
+            : base(n)
+        {
+        }
+
+        public override House Create()
+        {
+            return new WoodHouse();
+        }
+    } 
+    #endregion
+
+    internal abstract class House
+    {
+    }
+
+    internal class PanelHouse : House
+    {
+        public PanelHouse()
+        {
+            Console.WriteLine("Панельный дом построен");
+        }
+    }
+
+    internal class WoodHouse : House
+    {
+        public WoodHouse()
+        {
+            Console.WriteLine("Деревянный дом построен");
+        }
+    }
+}
