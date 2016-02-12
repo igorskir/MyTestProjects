bb92dc01f499e740cf74ad1fc1cdcefeec4195c6
Igor Skirnevskiy <igorskir@yandex.ru>
2016-02-12 09:04:11 +0600

    For teh prog

--- FOR_TEH_PROG/HelloWorldApplication/HelloWorldApplication/Program.cs
+++ FOR_TEH_PROG/HelloWorldApplication/HelloWorldApplication/Program.cs
@@ -11,11 +11,38 @@ namespace HelloWorldApplication
         static void Main(string[] args)
         {
             // Вывести пользователю простое сообщение.
+            SideEffect();
+
+            int i = 1;
+            int x = 10;
+            int a = x + ++x;
+            x = 10;
+            int b = ++x + x;
+            Console.WriteLine("a = {0}, b = {1}",a,b);
+
+            Console.WriteLine(++i);
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+
+
             Console.WriteLine("***** My First C# App *****");
             Console.WriteLine("Hello World!");
             Console.WriteLine();
             // Ожидать нажатия клавиши <Enter> перед завершением работы.
             Console.ReadLine();
         }
+
+        static public int F(ref int asd )
+        {
+           return asd = 5;
+        }
+        static public void SideEffect()
+        {
+            int a = 0, b = 0, c = 0;
+            a = 1; b = a + F(ref a);
+            a = 1; c = F(ref a) + a;
+            Console.WriteLine("a ={0}, b ={1}, c ={2}",a, b, c);
+        }
+
     }
 }
--- FileParsing/FileParsing/Program.cs
+++ FileParsing/FileParsing/Program.cs
@@ -7,19 +7,20 @@ using System.Threading.Tasks;
 
 namespace FileParsing
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            ParseMethodFirst(@"D:\data.txt", @"D:\res.txt");
-
+            //ParseMethodFirst(@"D:\data.txt", @"D:\res.txt");
+            //FindeNextId(@"D:\ids.txt");
+            GetPostElements(@"D:\post.txt");
             Console.ReadLine();
         }
 
         static void ParseMethodFirst(string filePath, string results)
         {
             StreamReader reader = File.OpenText(filePath);
-            string text = File.ReadAllText(filePath);
             string line;
             List<int> numbers = new List<int>();
             List<string> pathes = new List<string>();
@@ -41,6 +42,80 @@ namespace FileParsing
 
             foreach (var path in pathes) { Console.WriteLine(path); }
         }
+
+        static void FindeNextId(string filePath)
+        {
+            int id = 0;
+            List<int> idsInts = new List<int>();
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                string[] items = line.Split(' ');
+                int myId = int.Parse(items[0]);
+                idsInts.Add(myId);
+            }
+
+            for (int i = 1; i < idsInts.Count; i++)
+            {
+
+                if (idsInts[i] - idsInts[i - 1] != 1)
+                {
+                    Console.WriteLine("Id в списке идентификаторов находятся не в порядке увеления на 1");
+                    Console.WriteLine("Ошибка возникла между Id={0} и Id={1}", idsInts[i], idsInts[i - 1]);
+                    break;
+                }
+            }
+
+            int maxId = idsInts.Max();
+            id = maxId + 1;
+            Console.WriteLine("Максимальный найденный Id = {0}", maxId);
+            Console.WriteLine("Следующий Id = {0}", id);
+
+        }
+
+        
+        static void GetPostElements(string filePath)
+        {
+            List<Post> posts = new List<Post>();
+
+            StreamReader reader = File.OpenText(filePath);
+            string line;
+            List<int> numbers = new List<int>();
+            List<string> pathes = new List<string>();
+
+            string[] item = reader.ReadToEnd().Split('\n');
+            for (int i = 0; i < item.Length; i++)
+            {
+                item[i] = item[i].Trim('\r');
+            }
+
+            Post p1 = new Post();
+            p1.id = 10;
+            p1.name = "qwe";
+            p1.sername = "qweqwe";
+            for (int i = 0; i < item.Length; i+=3)
+            {
+                posts.Add(new Post() { id = int.Parse(item[i]), name = item[i+1], sername = item[i+2] });
+            }
+
+            foreach (var var in posts)
+            {
+                Console.WriteLine(var.id + " " + var.name + " " + var.sername);
+            }
+            //while ((line = reader.ReadToEnd()) != null)
+            //{
+
+            //    string[] items = line.Split('\n');
+            //    int myInt = int.Parse(items[1]);
+            //    numbers.Add(myInt);
+            //    foreach (var item in items)
+            //    {
+            //        if (item.StartsWith(@"item\") && item.EndsWith(@".ddj"))
+            //            pathes.Add(item);
+            //    }
+            //}
+
+        }
+
         static void ParseMethodSecond(string filePath, string results)
         {
             
@@ -62,5 +137,11 @@ namespace FileParsing
 
             foreach (var path in pathes) { Console.WriteLine(path); }
         }
+    }
+    struct Post
+    {
+        public int id;
+        public string name;
+        public string sername;
     }
 }
